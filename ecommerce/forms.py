b094715263caa30a447a2b59a37de4a310ddbf4b
Ygor Hora <ygor.hora@visagio.com>
2018-03-30 20:58:32 -0300

    changing widgets of forms in django

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -1,6 +1,24 @@
 from django import forms
 
 class ContactForm(forms.Form):
-	fullname = forms.CharField()
-	email = forms.EmailField()
-	content = forms.CharField()
+	fullname = forms.CharField(widget=forms.TextInput(
+			attrs={
+				"class":"form-control",
+				"placeholder":"Your fullname"
+			}
+		)
+	)
+	email = forms.EmailField(widget=forms.EmailInput(
+			attrs={
+				"class":"form-control",
+				"placeholder":"Your Email"
+			}
+		)
+	)
+	content = forms.CharField(widget=forms.Textarea(
+			attrs={
+				"class":"form-control",
+				"placeholder":"Your message"
+			}
+		)
+	)
