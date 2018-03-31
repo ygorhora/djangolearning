82e7a56666a5d8f4f46ff902576ab31ae9458a79
Ygor Hora <ygor.hora@visagio.com>
2018-03-30 21:07:11 -0300

    validating e-mail in form

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -18,17 +18,19 @@ def about_page(request):
 	return render(request, "home_page.html", context)
 
 def contact_page(request):
-	contact_form = ContactForm()
+	contact_form = ContactForm(request.POST or None)
 	context = {
 		"title":"Contact",
 		"content":"Welcome to the contact page.",
 		"form": contact_form,
 	}
-	if request.method == "POST":
-		print(request.POST)
-		print(request.POST.get('fullname'))
-		print(request.POST.get('email'))
-		print(request.POST.get('content'))
+	if contact_form.is_valid():
+		print(contact_form.cleaned_data)
+	# if request.method == "POST":
+	# 	print(request.POST)
+	# 	print(request.POST.get('fullname'))
+	# 	print(request.POST.get('email'))
+	# 	print(request.POST.get('content'))
 	return render(request, "contact/view.html", context)
 
 
