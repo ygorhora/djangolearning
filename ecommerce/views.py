1058e18a77b414496b7c6ba8ec72715a4f765393
Ygor Hora <ygor.hora@visagio.com>
2018-04-01 18:39:45 -0300

    login page with post request and simple authentication use case

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .forms import ContactForm
+from .forms import ContactForm, LoginForm
 
 def home_page(request):
 	context = {
@@ -32,6 +32,19 @@ def contact_page(request):
 	# 	print(request.POST.get('email'))
 	# 	print(request.POST.get('content'))
 	return render(request, "contact/view.html", context)
+
+def login_page(request):
+	form = LoginForm(request.POST or None)
+	print(request.user.is_authenticated())
+	if form.is_valid():
+		print(form.cleaned_data)
+	return render(request, "auth/login.html", {"form": form})
+
+def register_page(request):
+	form = LoginForm(request.POST or None)
+	if form.is_valid():
+		print(form.cleaned_data)
+	return render(request, "auth/register.html", {})
 
 
 def home_page_old(request):
